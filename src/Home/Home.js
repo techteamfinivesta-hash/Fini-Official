040796f903a297f0123ce5a345c52e3fd33ae126
Chehak <chehakagrawal01@gmail.com>
2025-11-07 20:26:49 +0530

    Modified about us section

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -118,8 +118,10 @@ const Home = () => {
               women-led organization that provides students with various
               opportunities that helps them to enhance their financial
               knowledge, gain practical insights through workshops, seminars,
-              events and competitions with the aim of encapsulating financial
-              principles, patience, risk management in bright young minds. We aim for excellence across all financial fields, helping students connect with key stakeholders while cultivating passion and self-investment in young minds.
+              events and competitions with the aim of encapsulating financial 
+              principles, patience, risk management in bright young minds. 
+              We aim for excellence across all financial fields, helping students 
+              connect with key stakeholders while cultivating passion and self-investment in young minds.
             </p>
           {/*
           <div className="col-2">
